b10ecf2b3c3ea75ec1fef3ae09beb64029a92f43
shivam kansliwal <skansliwal@gmail.com>
2022-11-25 22:44:53 +0530

    added implementation for list1.py

--- list1.py
+++ list1.py
@@ -1,6 +1,6 @@
 #duplicate element in a list
 #duplicate element in the perticular list
-list=[1,2,3,4,1,2,5,3,5]
+list=[1,2,3,4,1,2,5,3,5,100,100,51]
 result=[]
 for i in list:
     if i not in result:
